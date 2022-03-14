cd04888be71aaeec3adf110ffac2e1adad43397d
Payal Jindal <payal.jindal.19.1@mountblue.tech>
2022-03-14 20:31:18 +0530

    Edited map and flatten

--- flatten.js
+++ flatten.js
@@ -3,20 +3,20 @@
 
 function cb(element, depth){
     if (Array.isArray(element) && depth) {
-                  return cb(element[0], depth-1);
-              }
-              return element;
-          }
+        return cb(element[0], depth-1);
+    }
+    return element;
+}
 
 
 module.exports = function flatten(elements, depth){
     let arr=[];
       if (elements){
-          for (let index = 0; index < elements.length; index++){
+        for (let index = 0; index < elements.length; index++){
             arr.push(cb(elements[index], depth));
             }
-      return arr;
+        return arr;
       }
-      return [];
-      }
+    return [];
+}
 
--- map.js
+++ map.js
@@ -1,16 +1,13 @@
 
 let eachFunc = require('./each.js');
 
-/* To avoid this for loop, each function developed earlier is used
-    for (let index = 0; index < elements.length; index++){
-            newElements.push(cb(elements[index]));
-        }
-*/
 
 module.exports =function map(elements, cb){
     if (elements && cb){
         let newElements = [];
-        eachFunc.each(elements, func = (element, index, elements) => { newElements.push(cb(element, index, elements))});  
+        eachFunc.each(elements, func = (element, index, elements) => { 
+            newElements.push(cb(element, index, elements))
+        });  
         return newElements;
     }
     return [];
