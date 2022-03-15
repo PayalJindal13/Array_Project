532cbc0227d1c746c6be66d6209b9f1c9b333db2
Payal Jindal <payal.jindal.19.1@mountblue.tech>
2022-03-15 11:24:40 +0530

    Map file changed

--- map.js
+++ map.js
@@ -5,7 +5,7 @@ let eachFunc = require('./each.js');
 module.exports =function mapArray(elements, cb){
     if (elements && cb){
         let newElements = [];
-        eachFunc.each(elements, func = (element, index, elements) => { 
+        eachFunc(elements, func = (element, index, elements) => { 
             newElements.push(cb(element, index, elements))
         });  
         return newElements;
